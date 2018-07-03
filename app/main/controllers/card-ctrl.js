2393f57955311c0164d287aa16dba500ae68145b
Jorge Madson <filholouco@gmail.com>
2018-07-03 10:15:59 -0300

    infinite scroll não funcionando mas ordenando por nome

--- app/main/controllers/card-ctrl.js
+++ app/main/controllers/card-ctrl.js
@@ -1,23 +1,38 @@
 'use strict';
 angular.module('main')
   .controller('CardCtrl', function ($log, $http, Main, $scope) {
-
-    $scope.apiCards = {card: []};
     $log.log('CardCtrl in module main, controller:', this);
+
+    $scope.allCards = { card: [] };
+
+    //Salvar cartas localmente pra que seja lido só uma vez.
     $http.get('https://api.hearthstonejson.com/v1/latest/ptBR/cards.collectible.json')
       .then(function (response) {
         $log.log(response.data);
-        //response.data.slice(0,5);
-        $scope.response = response.data;
-        $scope.apiCards = response.data.slice(0, 15);
-      });
+        $scope.apiResponse = response.data;
 
+        $scope.allCards = response.data.slice(0, 15);
+        $scope.allCards.sort(porNome);
+      });
+    function porNome (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    }
     $scope.loadMore = function () {
       alert('Entrou no loadMore');
-      var qnt = $scope.apiCards.length;
+      var qnt = $scope.allCards.length;
       $log.log(qnt);
-      $scope.apiCards = $scope.response.slice(0, qnt + 5);
-      $scope.$broadcast('scroll.infiniteScrollComplete');
+      var cardsToAdd = $scope.apiResponse.slice(qnt - 1, 5);
+      $scope.allCards.push(cardsToAdd);
+      if ($scope.allCards.length === $scope.apiResponse.length) {
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      }
     };
     // $scope.$on('$stateChangeSuccess', function () {
     //   $scope.loadMore();
