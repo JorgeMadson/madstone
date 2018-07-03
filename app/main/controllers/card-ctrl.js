1b02c5f759efbce70106578bb7279ab7362c0d4f
Jorge Madson <filholouco@gmail.com>
2018-07-03 08:24:13 -0300

    infinite scroll so funciona qnd muda controller e no navegador

--- app/main/controllers/card-ctrl.js
+++ app/main/controllers/card-ctrl.js
@@ -2,25 +2,26 @@
 angular.module('main')
   .controller('CardCtrl', function ($log, $http, Main, $scope) {
 
+    $scope.apiCards = {card: []};
     $log.log('CardCtrl in module main, controller:', this);
     $http.get('https://api.hearthstonejson.com/v1/latest/ptBR/cards.collectible.json')
       .then(function (response) {
         $log.log(response.data);
         //response.data.slice(0,5);
         $scope.response = response.data;
-        $scope.apiCards = response.data.slice(0, 5);
+        $scope.apiCards = response.data.slice(0, 15);
       });
 
-    $scope.apiCards = [];
     $scope.loadMore = function () {
-      $log.log('Entrou no loadMore');
+      alert('Entrou no loadMore');
       var qnt = $scope.apiCards.length;
       $log.log(qnt);
       $scope.apiCards = $scope.response.slice(0, qnt + 5);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
     };
-    $scope.$on('$stateChangeSuccess', function () {
-      $scope.loadMore();
-    });
+    // $scope.$on('$stateChangeSuccess', function () {
+    //   $scope.loadMore();
+    // });
     $scope.temMais = function () {
       return true;
     };
