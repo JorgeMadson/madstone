050d657e4d4db47e84d94e05042db5bab0d11ece
Jorge Madson <filholouco@gmail.com>
2018-07-08 17:11:16 -0300

    Salvando as cartas localmente

--- app/main/controllers/card-ctrl.js
+++ app/main/controllers/card-ctrl.js
@@ -1,16 +1,23 @@
 'use strict';
 angular.module('main')
-  .controller('CardCtrl', function ($log, $http, Main, $scope) {
+  .controller('CardCtrl', function ($log, $http, Main, $scope, $localstorage) {
     $log.log('CardCtrl in module main, controller:', this);
 
+    $log.log($localstorage.getObject('cartasApi').length);
+
+    if (typeof $localstorage.getObject('cartasApi').length === 'undefined') {
+      $http.get('https://api.hearthstonejson.com/v1/latest/ptBR/cards.collectible.json')
+        .then(function (response) {
+          $log.log('pegou o json da api');
+          $log.log(response);
+          $scope.apiResponse = response.data;
+          $localstorage.setObject('cartasApi', $scope.apiResponse);
+        });
+    }
+
     $scope.allCards = [];
-    $scope.apiResponse = [];
-    //Salvar cartas localmente pra que seja lido só uma vez.
-    $http.get('https://api.hearthstonejson.com/v1/latest/ptBR/cards.collectible.json')
-      .then(function (response) {
-        $log.log(response.data);
-        $scope.apiResponse = response.data;
-      });
+    $scope.apiResponse = $localstorage.getObject('cartasApi').length ? $localstorage.getObject('cartasApi') : [];
+
     function porNome (a, b) {
       if (a.name > b.name) {
         return 1;
